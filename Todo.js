12b2ccfdbd30ea3b7c65871667cc6ab3c2ebf68a
Daniel R Meneses León <daniel.meneses@ironbit.com>
2019-06-12 09:51:50 -0500

    Getting data from HomeView to TodoView and displaying it in the header

--- Todo.js
+++ Todo.js
@@ -5,17 +5,22 @@ import store from './store';
 
 export default class Todo extends Component {
 
-    componentDidMount() {
-        // const something = this.props.navigation.getParams('name');
-        // console.log(something)
+    state = {
+        something: ''
     }
 
-    static navigationOptions = {
-        title: 'Home',
-      };    
+    componentDidMount() {
+        const something = this.props.navigation.getParam('name', 'deafult name');
+        this.setState({something})
+        console.log(something)
+    }
+
+    static navigationOptions = ({navigation}) => ({
+        title: navigation.getParam('name', 'default Title'),
+      });    
 
     render() {
-        
+        console.log('Inside render: ', this.state.something)
         const h = Dimensions.get('window').height;
         return (
             <View
